036db98330be8a3e1fa0ff921e395a786d4f7b19
Jack Bashford <jack.bashford42@gmail.com>
2019-01-25 15:20:06 +1100

    Add all files

---
+++ init.js
@@ -0,0 +1,8 @@
+SE.init({
+    clientId: 14291,
+    key: "F2QC3ry0mWbcCmkNxMsDVg((",
+    channelUrl: "https://userdata.jackbashford.com/channel",
+    complete: function(data) {
+        console.log(data);
+    }
+})
